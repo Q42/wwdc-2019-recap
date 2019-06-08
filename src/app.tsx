706ee6d61405cb1bce58db9122a035f5b609556b
Herman Banken <herman@q42.nl>
2019-06-07 22:54:34 -0700

    Bookmarking

--- src/app.tsx
+++ src/app.tsx
@@ -15,15 +15,25 @@ const videos = global.videos;
 
 class App extends React.Component {
 
-  public state = { filters: [] };
+  public state = { filters: [], bookmarks: [], showBookmarkedOnly: false };
 
-  toggleFilter(name: string) {
-    if (this.state.filters.indexOf(name) >= 0) {
+  toggleFilter(name: string, selected: boolean) {
+    if (!selected) {
       // remove
       this.setState({ filters: this.state.filters.filter((s) => s !== name) });
     } else {
       // add
       this.setState({ filters: [...this.state.filters, name] });
+    }
+  }
+
+  toggleBookmark(name: string, selected: boolean) {
+    if (!selected) {
+      // remove
+      this.setState({ bookmarks: this.state.bookmarks.filter((s) => s !== name) });
+    } else {
+      // add
+      this.setState({ bookmarks: [...this.state.bookmarks, name] });
     }
   }
   
@@ -34,12 +44,18 @@ class App extends React.Component {
       ...tags[tagName],
     }));
 
+    tagObjects.push({
+      id: "bookmark",
+      selected: this.state.filters.indexOf("bookmark") >= 0,
+      name: "Bookmarks",
+    })
+
     tagObjects.unshift({ id: "all", name: "All", selected: this.state.filters.length === 0 || tagObjects.length === this.state.filters.length });
 
     return <div>
       <div className="controls">{Controls(tagObjects, this.toggleFilter.bind(this))}</div>
       <div className="videos">{
-        videos.map((video) => (Video(video)))
+        videos.map((video) => (Video({ ...video, bookmarked: this.state.bookmarks.indexOf(video.id) >= 0 }, this.toggleBookmark.bind(this))))
       }</div>
     </div>;
   }
