cf952be88669d32dbab2d8e5a5ed65ff3b10db3d
Herman Banken <herman@q42.nl>
2019-06-07 23:40:56 -0700

    Style videos in blocks

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,9 +1,10 @@
-export default (video: any, toggle: (id: string) => void) => (<div key={video.id}>
+export default (video: any, toggle: (id: string) => void) => (<div className="video" key={video.id}>
     <h2>
         {video.id} - {video.title} <a href={video.webPermalink}>(video)</a><span> </span>
         <label className="bookmark">Bookmark <input onChange={(e) => toggle(video.id, e.target.checked)} type="checkbox" checked={video.bookmarked} /></label>
     </h2>
     <div>{video.description}</div>
     <div>Keywords: {(video.keywords || []).map((key) => (<span className="keyword" key={key}>{key}</span> ))}</div>
+    <div>Platforms: {(video.platforms || []).map((key) => (<span className="keyword" key={key}>{key}</span> ))}</div>
 </div>)
   
